07b7497fcca2948701550373d0f6721562a1efe9
TE-Fire <3037749727@qq.com>
2026-06-17 22:55:12 +0800

    feat(auth-controller): 新增用户实体类和测试接口2
    新增User实体类用于封装用户信息，同时在TestController中添加带操作日志注解的test2接口，支持返回用户数据

--- bluenote/bluenote-auth/src/main/java/com/tefire/auth/controller/TestController.java
+++ bluenote/bluenote-auth/src/main/java/com/tefire/auth/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.tefire.auth.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tefire.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.tefire.framework.common.response.Response;
 
 /*
@@ -13,7 +16,17 @@ import com.tefire.framework.common.response.Response;
 @RestController
 public class TestController {
     @GetMapping("/test")
+    @ApiOperationLog(description = "测试接口")
     public Response<String> test() {
         return Response.success("Hello, 犬小哈专栏");
+    }
+
+    @GetMapping("/test2")
+    @ApiOperationLog(description = "测试接口2")
+    public Response<User> test2() {
+        return Response.success(User.builder()
+                        .nickName("犬小哈")
+                        .createTime(LocalDateTime.now())
+                        .build());
     }
 }
