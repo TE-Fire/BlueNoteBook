66869ecdb6a034681a973d4e8a777b6c88929c8c
TE-Fire <3037749727@qq.com>
2026-06-22 21:40:27 +0800

    feat(gateway): add bluenote gateway module and basic configuration
    新增网关服务模块，配置了端口8000、Nacos服务发现以及路由转发规则，将/auth/**请求转发到bluenote-auth服务并去掉前缀

---
+++ bluenote/bluenote-gateway/src/main/java/com/tefire/BlueNoteGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.tefire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Author: TE-Fire 3037749727@qq.com
+ * @Date: 2026-06-22 20:57:28
+ * @Description: 网关服务启动类
+ */
+@SpringBootApplication
+public class BlueNoteGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueNoteGatewayApplication.class, args);
+    }
+}
