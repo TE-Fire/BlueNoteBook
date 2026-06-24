9771fc460324f77687b4aa5668576006cb1c70d7
TE-Fire <3037749727@qq.com>
2026-06-24 23:27:54 +0800

    feat(oss): add oss module and related configurations
    1. add bluenote-oss module to parent pom
    2. create oss multi-module project structure: oss-api and oss-biz
    3. add oss service startup class and basic application config
    4. configure logback for auth module and oss-biz module
    5. add vscode debug launch config for oss-biz

---
+++ bluenote/bluenote-oss/bluenote-oss-biz/src/main/java/com/tefire/BlueNoteOssBizApplication.java
@@ -0,0 +1,17 @@
+package com.tefire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Author: TE-Fire 3037749727@qq.com
+ * @Date: 2026-06-24 23:15:32
+ * @Description: oss 服务启动类
+ */
+@SpringBootApplication
+public class BlueNoteOssBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueNoteOssBizApplication.class, args);
+    }
+}
