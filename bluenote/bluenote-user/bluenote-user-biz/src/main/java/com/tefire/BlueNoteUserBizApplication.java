b6a05042275604c56f1280b39cfae90b6a2c90c1
TE-Fire <3037749727@qq.com>
2026-06-25 20:49:18 +0800

    feat(user): 新增用户服务模块及基础配置
    1. 新增bluenote-user父模块及api、biz子模块
    2. 添加用户服务启动类、配置文件、日志配置
    3. 配置Nacos服务发现与本地开发环境
    4. 新增全局异常处理器和响应状态码枚举
    5. 新增VSCode调试配置并更新根模块pom

---
+++ bluenote/bluenote-user/bluenote-user-biz/src/main/java/com/tefire/BlueNoteUserBizApplication.java
@@ -0,0 +1,16 @@
+package com.tefire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Author: TE-Fire 3037749727@qq.com
+ * @Date: 2026-06-25 20:30:41
+ * @Description: 用户服务启动类
+ */
+@SpringBootApplication
+public class BlueNoteUserBizApplication {
+     public static void main(String[] args) {
+        SpringApplication.run(BlueNoteUserBizApplication.class, args);
+    }
+}
