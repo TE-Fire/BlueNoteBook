0e2a28a6f0fa177e863c015dabb905785278cdfc
TE-Fire <3037749727@qq.com>
2026-06-29 19:35:36 +0800

    feat: add distributed id generator module
    add bluenote-distributed-id-generator multi-module project, including api and biz layers, add launch config for it in vscode, and configure basic spring boot and nacos settings

---
+++ bluenote/bluenote-distributed-id-generator/bluenote-distributed-id-generator-biz/src/main/java/com/tefire/BlueNoteDistributedIdGeneratorBizApplication.java
@@ -0,0 +1,16 @@
+package com.tefire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Author: TE-Fire 3037749727@qq.com
+ * @Date: 2026-06-29 19:29:24
+ * @Description: 启动类
+ */
+@SpringBootApplication
+public class BlueNoteDistributedIdGeneratorBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlueNoteDistributedIdGeneratorBizApplication.class, args);
+    }
+}
