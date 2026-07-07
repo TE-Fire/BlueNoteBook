543bde66090be4466cd2f39ca65c2ac083d97cb4
TE-Fire <3037749727@qq.com>
2026-07-07 16:40:47 +0800

    feat(gateway&user-relation): add user relation service route and enable feign client
    1. 为网关配置添加用户关系服务的路由规则，匹配/relation/**请求并StripPrefix=1
    2. 在用户关系业务启动类上开启Feign客户端扫描，指定扫描com.tefire.user.api包

--- bluenote/bluenote-user-relation/bluenote-user-relation-biz/src/main/java/com/tefire/BlueNoteUserRelationBizApplication.java
+++ bluenote/bluenote-user-relation/bluenote-user-relation-biz/src/main/java/com/tefire/BlueNoteUserRelationBizApplication.java
@@ -3,6 +3,7 @@ package com.tefire;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /*
  * @Author: TE-Fire 3037749727@qq.com
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.tefire.relation.domain.mapper")
+@EnableFeignClients("com.tefire.user.api")
 public class BlueNoteUserRelationBizApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlueNoteUserRelationBizApplication.class, args);
