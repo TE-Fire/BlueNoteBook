5c217327e7bb0419b50cd6c8fdacb9087608d4b7
TE-Fire <3037749727@qq.com>
2026-07-13 15:33:47 +0800

    feat(common): add分页响应工具类PageResponse
    新增通用分页响应实体类，封装分页相关参数并提供快速构建成功响应的静态方法，支持自定义每页数量

---
+++ bluenote/bluenote-framework/bluenote-common/src/main/java/com/tefire/framework/common/response/PageResponse.java
@@ -0,0 +1,58 @@
+package com.tefire.framework.common.response;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/*
+ * @Author: TE-Fire 3037749727@qq.com
+ * @Date: 2026-07-13 14:59:52
+ * @Description: 分页响应参数工具类
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class PageResponse<T> extends Response<List<T>> {
+
+    private long pageNo; // 当前页码
+    private long totalCount; // 总数据量
+    private long pageSize; // 每页展示的数据量
+    private long totalPage; // 总页数
+
+    public static <T> PageResponse<T> success(List<T> data, long pageNo, long totalCount) {
+        PageResponse<T> pageResponse = new PageResponse<>();
+        pageResponse.setSuccess(true);
+        pageResponse.setData(data);
+        pageResponse.setPageNo(pageNo);
+        pageResponse.setTotalCount(totalCount);
+        // 每页展示的数据量
+        long pageSize = 10L;
+        pageResponse.setPageSize(pageSize);
+        // 计算总页数
+        long totalPage = (totalCount + pageSize - 1) / pageSize;
+        pageResponse.setTotalPage(totalPage);
+        return pageResponse;
+    }
+
+    public static <T> PageResponse<T> success(List<T> data, long pageNo, long totalCount, long pageSize) {
+        PageResponse<T> pageResponse = new PageResponse<>();
+        pageResponse.setSuccess(true);
+        pageResponse.setData(data);
+        pageResponse.setPageNo(pageNo);
+        pageResponse.setTotalCount(totalCount);
+        pageResponse.setPageSize(pageSize);
+        // 计算总页数
+        long totalPage = pageSize == 0 ? 0 : (totalCount + pageSize - 1) / pageSize;
+        pageResponse.setTotalPage(totalPage);
+        return pageResponse;
+    }
+
+    /**
+     * 获取总页数
+     * @return
+     */
+    public static long getTotalPage(long totalCount, long pageSize) {
+        return pageSize == 0 ? 0 : (totalCount + pageSize - 1) / pageSize;
+    }
+
+}
