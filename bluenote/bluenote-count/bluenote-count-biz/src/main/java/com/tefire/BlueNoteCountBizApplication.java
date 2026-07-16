3932684d559e36be8654850ccc740506fbc21e81
TE-Fire <3037749727@qq.com>
2026-07-16 19:49:48 +0800

    feat(count): 新增计数服务模块
    新增 bluenote-count 模块，包含 api 和 biz 子模块，添加必要的配置文件，如 application.yml、bootstrap.yml、logback 配置和 mybatis generator 配置，并在根 pom.xml 中注册该模块。

---
+++ bluenote/bluenote-count/bluenote-count-biz/src/main/java/com/tefire/BlueNoteCountBizApplication.java
@@ -0,0 +1,11 @@
+package com.tefire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueNoteCountBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlueNoteCountBizApplication.class, args);
+    }
+}
