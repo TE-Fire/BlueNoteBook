9afc9ed6a8a839e429aa2af69413431402a2d4af
TE-Fire <3037749727@qq.com>
2026-07-17 12:43:45 +0800

    feat(count-biz): 引入buffer-trigger实现MQ消息批量消费
    新增快手Buffer Trigger依赖，改造粉丝计数消费者实现消息批量聚合消费，配置了队列容量、批次大小和刷新延迟参数

--- bluenote/bluenote-count/bluenote-count-biz/src/main/java/com/tefire/count/consumer/CountFansConsumer.java
+++ bluenote/bluenote-count/bluenote-count-biz/src/main/java/com/tefire/count/consumer/CountFansConsumer.java
@@ -1,10 +1,15 @@
 package com.tefire.count.consumer;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
+import com.github.phantomthief.collection.BufferTrigger;
 import com.tefire.count.constant.MQConstants;
+import com.tefire.framework.common.util.JsonUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,8 +25,23 @@ import lombok.extern.slf4j.Slf4j;
 )
 public class CountFansConsumer implements RocketMQListener<String> {
     
+    private BufferTrigger<String> bufferTrigger = BufferTrigger.<String>batchBlocking()
+            .bufferSize(50000) // 缓存队列的最大容量
+            .batchSize(1000)   // 一批次最多聚合 1000 条
+            .linger(Duration.ofSeconds(1)) // 多久聚合一次
+            .setConsumerEx(this::consumeMessage)
+            .build();
+
     @Override
     public void onMessage(String body) {
         log.info("## 消费到了 MQ 【计数: 粉丝数】, {}...", body);
+        // 往 bufferTrigger 中添加元素
+        bufferTrigger.enqueue(body);
+
+    }
+
+    private void consumeMessage(List<String> bodys) {
+        log.info("==> 聚合消息, size: {}", bodys.size());
+        log.info("==> 聚合消息, {}", JsonUtils.toJsonString(bodys));
     }
 }
