d521c9913b5d34d7ad2d9484add26aeef2211fff
TE-Fire <3037749727@qq.com>
2026-07-19 11:14:07 +0800

    refactor(note service): 调整布隆过滤器初始化逻辑与方法命名
    优化点赞业务中的布隆过滤器处理逻辑，将异步初始化统一调整为按需执行，并重命名方法消除歧义

--- bluenote/bluenote-note/bluenote-note-biz/src/main/java/com/tefire/note/biz/service/impl/NoteServiceImpl.java
+++ bluenote/bluenote-note/bluenote-note-biz/src/main/java/com/tefire/note/biz/service/impl/NoteServiceImpl.java
@@ -599,11 +599,15 @@ public class NoteServiceImpl implements NoteService {
                 long expireSeconds = 60*60*24 + RandomUtil.randomInt(60*60*24);
 
                 if (count > 0) {
-                    asynBatchAddNoteLike2BloomAndExpire(userId, expireSeconds, bloomUserNoteLikeListKey);
+                    // 异步初始化布隆过滤器
+                    threadPoolTaskExecutor.submit(() ->
+                            batchAddNoteLike2BloomAndExpire(userId, expireSeconds, bloomUserNoteLikeListKey));
                     throw new BizException(ResponseCodeEnum.NOTE_ALREADY_LIKED);
                 }
+                
+                // 若目标笔记未被点赞，查询当前用户是否有点赞其他笔记，有则同步初始化布隆过滤器
+                batchAddNoteLike2BloomAndExpire(userId, expireSeconds, bloomUserNoteLikeListKey);
 
-                // 若数据库中也没有点赞记录，说明该用户还未点赞过任何笔记
                 // Lua 脚本路径
                 script.setScriptSource(new ResourceScriptSource(new ClassPathResource("/lua/bloom_add_note_like_and_expire.lua")));
                 // 返回值类型
@@ -779,7 +783,7 @@ public class NoteServiceImpl implements NoteService {
      * @param expireSeconds
      * @param bloomUserNoteLikeListKey
      */
-    private void asynBatchAddNoteLike2BloomAndExpire(Long userId, long expireSeconds, String bloomUserNoteLikeListKey) {
+    private void batchAddNoteLike2BloomAndExpire (Long userId, long expireSeconds, String bloomUserNoteLikeListKey) {
         threadPoolTaskExecutor.submit(() -> {
             try {
                 // 异步全量同步，并设置过期时间
